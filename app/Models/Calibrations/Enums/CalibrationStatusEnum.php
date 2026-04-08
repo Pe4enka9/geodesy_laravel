9887136ca222f8428331d1a4d614b02a63ff87fb
Ivan <ivan.tixonov00@mail.ru>
2026-04-08 21:54:31 +0700

    переменные scss

--- app/Models/Calibrations/Enums/CalibrationStatusEnum.php
+++ app/Models/Calibrations/Enums/CalibrationStatusEnum.php
@@ -6,14 +6,12 @@ enum CalibrationStatusEnum: string
 {
     case ACTIVE = 'active';
     case EXPIRED = 'expired';
-    case VOIDED = 'voided';
 
     public function label(): string
     {
         return match ($this) {
             self::ACTIVE => 'Активный',
             self::EXPIRED => 'Истекший',
-            self::VOIDED => 'Аннулированный',
         };
     }
 }
